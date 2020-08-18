4435332eb3e40a021e1b0f71d8f37a8e63b7f165
Colin Swinney <colinjswinney@gmail.com>
2020-08-17 23:22:14 -0500

    browse, header, and loading component, completed through "Hydrating Our Firestore with Data" screencast

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles';
 import { App } from './app';
 import { FirebaseContext } from './context/firebase';
-import { config } from './constants/firebase-config'
+import { config } from './constants/firebase-config';
+
+// import { seedDatabase } from './seed';
 
 const firebase = window.firebase.initializeApp(config);
+// seedDatabase(firebase);
 
 ReactDOM.render(
-    <>
-        <FirebaseContext.Provider value={{ firebase: window.firebase }}>
-            <GlobalStyles />
-            <App />
-        </FirebaseContext.Provider>
-    </>, 
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+        <GlobalStyles />
+        <App />
+    </FirebaseContext.Provider>, 
     document.getElementById('root')
 );
