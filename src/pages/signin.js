4d4ae9218a9ad9273d2f514d0934f78dd0d78353
Colin Swinney <colinjswinney@gmail.com>
2020-08-18 14:35:47 -0500

    final application after completing course

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -6,7 +6,6 @@ import { HeaderContainer } from '../containers/header';
 import { FooterContainer } from '../containers/footer';
 import * as ROUTES from '../constants/routes';
 
-
 export default function Signin() {
     const history = useHistory();
     const { firebase } = useContext(FirebaseContext);
@@ -15,7 +14,7 @@ export default function Signin() {
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
     
-    const isInvalid = password === '' || emailAddress === '';
+    const isInvalid = password === '' | emailAddress === '';
     
     const handleSignin = (event) => {
         event.preventDefault();
@@ -24,48 +23,49 @@ export default function Signin() {
             .auth()
             .signInWithEmailAndPassword(emailAddress, password)
             .then(() => {
-                setEmailAddress('');
-                setPassword('');
-                setError('');
                 history.push(ROUTES.BROWSE);
             })
-            .catch((error) => setError(error.message));
+            .catch((error) => {
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message)
+            });
     }
     
     return (
         <>
-            <HeaderContainer>
-                <Form>
-                    <Form.Title>Sign In</Form.Title>
-                    {error && <Form.Error>{error}</Form.Error>}
+        <HeaderContainer>
+            <Form>
+                <Form.Title>Sign In</Form.Title>
+                {error && <Form.Error>{error}</Form.Error>}
+                
+                <Form.Base onSubmit={handleSignin} method="POST">
+                    <Form.Input
+                        placeholder="Email address"
+                        value={emailAddress}
+                        onChange={({ target }) => setEmailAddress(target.value)}
+                    />
+                    <Form.Input
+                        type="password"
+                        value={password}
+                        autoComplete="off"
+                        placeholder="Password"
+                        onChange={({ target }) => setPassword(target.value)} 
+                    />
+                    <Form.Submit disabled={isInvalid} type="submit">
+                        Sign In
+                    </Form.Submit>
                     
-                    <Form.Base onSubmit={handleSignin} method="POST">
-                        <Form.Input
-                            placeholder="Email address"
-                            value={emailAddress}
-                            onChange={({ target }) => setEmailAddress(target.value)}
-                        />
-                        <Form.Input
-                            type="password"
-                            value={password}
-                            autoComplete="off"
-                            placeholder="Password"
-                            onChange={({ target }) => setPassword(target.value)} 
-                        />
-                        <Form.Submit disabled={isInvalid} type="submit">
-                            Sign In
-                        </Form.Submit>
-                        
-                        <Form.Text>
-                            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
-                        </Form.Text>    
-                        <Form.TextSmall>
-                            This page is protected by Google reCAPTCHA.
-                        </Form.TextSmall>
-                    </Form.Base>
-                </Form>
-            </HeaderContainer>
-            <FooterContainer />
+                    <Form.Text>
+                        New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+                    </Form.Text>    
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA.
+                    </Form.TextSmall>
+                </Form.Base>
+            </Form>
+        </HeaderContainer>
+        <FooterContainer />
         </>
     )
 }
