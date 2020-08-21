5ef84ea01f3a28c52222605e1a7d325adbdf35df
Colin Swinney <colinjswinney@gmail.com>
2020-08-20 23:27:51 -0500

    Updates

---
+++ precache-manifest.db3e6322af50e32df9dd0a9e0eb2a83f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690055d7ee7d2619d7ec14aff7dd5cd4",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "0eda1051ed652c811255",
+    "url": "/netflix-clone/static/js/2.2da8f62f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix-clone/static/js/2.2da8f62f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e04f3957f23eea335f",
+    "url": "/netflix-clone/static/js/main.50c46b88.chunk.js"
+  },
+  {
+    "revision": "ed067e929af6839177a1",
+    "url": "/netflix-clone/static/js/runtime-main.bbcab00e.js"
+  }
+]);
