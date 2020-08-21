0f2431bc4280802691733cb7a183f12acd7ac287
Colin Swinney <colinjswinney@gmail.com>
2020-08-20 23:36:47 -0500

    gh pages deploy

---
+++ src/constants/firebase-config.js
@@ -0,0 +1,10 @@
+export const config = {
+    apiKey: "AIzaSyA2JtTn-fdBW5un2gXJkJI6dLPDTMexbCY",
+    authDomain: "netflix-clone-7c423.firebaseapp.com",
+    databaseURL: "https://netflix-clone-7c423.firebaseio.com",
+    projectId: "netflix-clone-7c423",
+    storageBucket: "netflix-clone-7c423.appspot.com",
+    messagingSenderId: "729285340705",
+    appId: "1:729285340705:web:4156b0430fa148d684bd60",
+    measurementId: "G-DBDY3GBGF7"
+}
